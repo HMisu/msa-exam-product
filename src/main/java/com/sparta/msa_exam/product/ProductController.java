5e8214292958d615a691000da48d7e39bb7a0eb3
misu <misuhan33@gmail.com>
2024-12-02 22:59:18 +0900

    init commit

---
+++ src/main/java/com/sparta/msa_exam/product/ProductController.java
@@ -0,0 +1,13 @@
+package com.sparta.msa_exam.product;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+}
+
